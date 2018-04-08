8baa1929472f92758bcd4f5583d27fa53ed95a00
derrick-long <long.derrick8@gmail.com>
2018-04-08 18:03:57 -0400

    fix passport config

--- routes/auth.js
+++ routes/auth.js
@@ -6,5 +6,10 @@ const passport = require('passport');
 router.get('/google',passport.authenticate('google',
 {scope: ['profile', 'email']}));
 
+router.get('/google/callback',
+  passport.authenticate('google', {failureRedirect: '/'}),
+(req,res)=> {
+  res.redirect('/dashboard');
+});
 
 module.exports = router;
