227e3fe9cf90ad5667fe37e1c5c381f5f0ec8827
derrick-long <long.derrick8@gmail.com>
2018-04-17 10:30:36 -0400

    add game model skeleton

---
+++ models/game.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Game Schema
+// time
+// date
+// type
+// 
+
+
+
+
+const GameSchema = new Schema ({
+
+});
+
+mongoose.model('games', GameSchema, 'games');
---
+++ routes/game.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const mongoose = require('mongoose');
+const Game = mongoose.model('games');
+const User = mongoose.model('users');
+
+
+/// route for games, probably want sorted by most recent by default
+// or closest? 
