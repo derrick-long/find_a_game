f07914c70c485a5ec795ac636a802aee24f86246
derrick-long <long.derrick8@gmail.com>
2018-06-04 21:59:53 -0400

    start working on stars feature for reviews

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,1 +1,19 @@
-/// come back to this add active tabbing 
+const ratings = {
+  placeholder: 4.7
+};
+
+//total stars
+const starsTotal = 5;
+
+
+//run getratings when Dom loads
+// get ratings
+
+function getRatings() {
+  console.log('ran');
+}
+
+//
+//
+document.addEventListener('DOMContentLoaded',
+getRatings);
