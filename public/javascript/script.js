cb3cdd58a8cd4dbb646002aa2417be583b459449
derrick-long <long.derrick8@gmail.com>
2018-06-11 11:39:39 -0400

    working on star css

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,30 +1,24 @@
 // like the css only approach but need only whole numbers
 // replace objects with the user/player object, only allow  them to review once per game
 
+if ($(".profile")[0]){
+  console.log('stuff');
+} else {
+  console.log('not here');
+}
 
-//so we need to make mongodb objects available here in order to DISPLAY our ratings, or maybe just move this stuff
-//into a route
 
 
-// Initial Ratings
 
-//this functionality but replace these ratings with values stored on users
-    const ratings = {
-      demo: 3
-    };
-
-    // Total Stars
-    const starsTotal = 5;
-
-    // Run getRatings when DOM loads
-    document.addEventListener('DOMContentLoaded', getRatings);
-
-    // Form Elements
-    const productSelect = document.getElementById('product-select');
-    const ratingControl = document.getElementById('rating-control');
-
-    // Init product
-    let product;
+    // // Run getRatings when DOM loads
+    // document.addEventListener('DOMContentLoaded', getRatings);
+    //
+    // // Form Elements
+    // const productSelect = document.getElementById('product-select');
+    // const ratingControl = document.getElementById('rating-control');
+    //
+    // // Init product
+    // let product;
 
     // Product select change
     // productSelect.addEventListener('change', (e) => {
@@ -51,18 +45,18 @@
     // });
 
     // Get ratings
-    function getRatings() {
-      for (let rating in ratings) {
-        // Get percentage
-        const starPercentage = (ratings[rating] / starsTotal) * 100;
-
-        // Round to nearest 10
-        const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
-
-        // Set width of stars-inner to percentage
-        document.querySelector(`.${rating} .stars-inner`).style.width = starPercentageRounded;
-
-        // Add number rating
-        document.querySelector(`.${rating} .number-rating`).innerHTML = ratings[rating];
-      }
-    }
+    // function getRatings() {
+    //   for (let rating in ratings) {
+    //     // Get percentage
+    //     const starPercentage = (ratings[rating] / starsTotal) * 100;
+    //
+    //     // Round to nearest 10
+    //     const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
+    //
+    //     // Set width of stars-inner to percentage
+    //     document.querySelector(`.${rating} .stars-inner`).style.width = starPercentageRounded;
+    //
+    //     // Add number rating
+    //     document.querySelector(`.${rating} .number-rating`).innerHTML = ratings[rating];
+    //   }
+    // }
