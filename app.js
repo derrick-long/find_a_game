c437625bf9ddf3e711cdcac4a2a4b6bd08075d08
derrick-long <long.derrick8@gmail.com>
2018-07-01 13:36:58 -0400

    add truncate function to avoid terrible index

--- app.js
+++ app.js
@@ -88,6 +88,19 @@ app.use((req, res, next)=> {
   next();
 });
 
+
+//ejs helper
+app.locals.truncate = function(str, len){
+    if (str.length > len && str.length > 0) {
+			var new_str = str + " ";
+			new_str = str.substr(0, len);
+			new_str = str.substr(0, new_str.lastIndexOf(" "));
+			new_str = (new_str.length > 0) ? new_str : str.substr(0, len);
+			return new_str + '...';
+		}
+		return str;
+};
+
 //pluralize helper
 
 app.locals.makePlural = function(item, number){
