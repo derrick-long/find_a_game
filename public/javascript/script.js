66dddcc2b2dae5da1fcb5c1f1046c3e50e1a84bb
derrick-long <long.derrick8@gmail.com>
2018-07-04 23:15:45 -0400

    google maps

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -61,11 +61,44 @@ $( document ).ready(function() {
 });
 
 //maps stuff
-//add if only run on pages with the map element 
-function initMap(){
-  var options = {
-    zoom:8,
-    center: {lat: 42.3601, lng: -71.0895}
-  };
-  var map = new google.maps.Map(document.getElementById('map'), options);
-}
+//add if only run on pages with the map element
+var map;
+   var infowindow;
+
+   function initMap() {
+     var pyrmont = {lat: -33.867, lng: 151.195};
+
+     map = new google.maps.Map(document.getElementById('map'), {
+       center: pyrmont,
+       zoom: 15
+     });
+
+     infowindow = new google.maps.InfoWindow();
+     var service = new google.maps.places.PlacesService(map);
+     service.nearbySearch({
+       location: pyrmont,
+       radius: 500,
+       type: ['store']
+     }, callback);
+   }
+
+   function callback(results, status) {
+     if (status === google.maps.places.PlacesServiceStatus.OK) {
+       for (var i = 0; i < results.length; i++) {
+         createMarker(results[i]);
+       }
+     }
+   }
+
+   function createMarker(place) {
+     var placeLoc = place.geometry.location;
+     var marker = new google.maps.Marker({
+       map: map,
+       position: place.geometry.location
+     });
+
+     google.maps.event.addListener(marker, 'click', function() {
+       infowindow.setContent(place.name);
+       infowindow.open(map, this);
+     });
+   }
