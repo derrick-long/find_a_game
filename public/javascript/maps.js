ad57f6f99c8db81c96bbb1fd0613566112eb0007
derrick-long <long.derrick8@gmail.com>
2018-07-28 23:43:10 -0400

    work with geocoding on maps

--- public/javascript/maps.js
+++ public/javascript/maps.js
@@ -1,1 +1,29 @@
- // add code to make run only if map div is present
+var map;
+var marker;
+
+function initMap() {
+  var mapOptions = {
+    center: new google.maps.LatLng(40.680898,-8.684059),
+    zoom: 11,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+}
+google.maps.event.addDomListener(window, "load", initialize);
+
+function searchAddress(){
+  var addressInput = document.getElementbyId('map-search').value;
+  var geocoder = new google.maps.Geocoder();
+
+  geocoder.geocode({address: addressInput}, function(results,status){
+    if (status == google.maps.GeocoderStatus.OK) {
+
+    }
+  }
+}
+
+
+
+if ($("#map")[0]){
+  initMap();
+}
