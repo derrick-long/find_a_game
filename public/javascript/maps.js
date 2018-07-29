4ef38fbbd0d5c07e1f64f272926301bfacdfcc9e
derrick-long <long.derrick8@gmail.com>
2018-07-28 23:56:02 -0400

    break geocoding

--- public/javascript/maps.js
+++ public/javascript/maps.js
@@ -12,18 +12,35 @@ function initMap() {
 google.maps.event.addDomListener(window, "load", initialize);
 
 function searchAddress(){
+
   var addressInput = document.getElementbyId('map-search').value;
   var geocoder = new google.maps.Geocoder();
 
   geocoder.geocode({address: addressInput}, function(results,status){
     if (status == google.maps.GeocoderStatus.OK) {
 
+      var myResult = results[0].geometry.location;
+
+      map.setCenter(myResult);
+
+      map.setZoom(15);
+    } else { // if status value is not equal to "google.maps.GeocoderStatus.OK"
+
+      alert("The Geocode was not successful for the following reason: " + status);
+
     }
-  }
+  });
 }
 
+function createMarker(latlng) {
+  //clear old marker
+  if (marker != undefined && marker != '') {
+    marker.setMap(null);
+    marker ='';
+  }
 
-
-if ($("#map")[0]){
-  initMap();
+  marker = new google.maps.Marker({
+    map:map,
+    position: latlng
+  });
 }
