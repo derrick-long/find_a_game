d1ee5a0b7f3a1ed8943eb6adfecea3fc7211fe24
derrick-long <long.derrick8@gmail.com>
2018-07-29 19:16:00 -0400

    google maps geocoding functional

--- public/javascript/maps.js
+++ public/javascript/maps.js
@@ -1,14 +1,22 @@
 var map;
 var marker;
 
+
+
 function initMap() {
+
   var mapOptions = {
     center: new google.maps.LatLng(40.680898,-8.684059),
     zoom: 11,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+   map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
+
+
+google.maps.event.addDomListener(window, 'load', initMap);
+
 
 
 function searchAddress(){
@@ -16,7 +24,7 @@ function searchAddress(){
   var addressInput = document.getElementById('map-search').value;
   var geocoder = new google.maps.Geocoder();
 
-  geocoder.geocode({address: addressInput}, function(results,status){
+  geocoder.geocode({address: addressInput}, function(results, status){
     if (status == google.maps.GeocoderStatus.OK) {
 
       var myResult = results[0].geometry.location;
@@ -33,7 +41,7 @@ function searchAddress(){
 }
 
 
-google.maps.event.addDomListener(window, "load", initMap);
+
 
 function createMarker(latlng) {
   //clear old marker
@@ -43,7 +51,7 @@ function createMarker(latlng) {
   }
 
   marker = new google.maps.Marker({
-    map:map,
+    map: map,
     position: latlng
   });
 }
