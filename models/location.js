45c2c63d095bb7f448944fbc30be1bd65739a5e8
derrick-long <long.derrick8@gmail.com>
2018-08-05 15:41:18 -0400

    geloc mongoose updates

--- models/location.js
+++ models/location.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const locationSchema = new Schema ({
 
-const LocationSchema = new Schema({
-
-
-
-
+  location: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true
+    },
+  },
+  coordinates: {
+    type: [Number],
+    default: [0,0],
+    required: true
+  }
 
 });
