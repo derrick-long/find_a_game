82207d386df0a6854c484d0a9ba88a90d19a3ab8
derrick-long <long.derrick8@gmail.com>
2018-08-25 18:05:04 -0400

    data from backend  to front end

--- public/javascript/maps.js
+++ public/javascript/maps.js
@@ -1,5 +1,5 @@
 
-
+//maybe change event to onload? use document.ready probably?
 
 function initMap() {
 
@@ -7,24 +7,21 @@ function initMap() {
     zoom: 8,
     center: {lat: -34.397, lng: 150.644}
   });
-  var geocoder = new google.maps.Geocoder();
 
-  document.getElementById('submit').addEventListener('click', function() {
-    geocodeAddress(geocoder, map);
-  });
 }
 
-function geocodeAddress(geocoder, resultsMap) {
-  var address = document.getElementById('map-search').value;
-  geocoder.geocode({'address': address}, function(results, status) {
-    if (status === 'OK') {
-      resultsMap.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}
+
+// function geocodeAddress(geocoder, resultsMap) {
+//   var address = document.getElementById('map-search').value;
+//   geocoder.geocode({'address': address}, function(results, status) {
+//     if (status === 'OK') {
+//       resultsMap.setCenter(results[0].geometry.location);
+//       // var marker = new google.maps.Marker({
+//       //   map: resultsMap,
+//       //   position: results[0].geometry.location
+//       // });
+//     } else {
+//       alert('Geocode was not successful for the following reason: ' + status);
+//     }
+//   });
+// }
