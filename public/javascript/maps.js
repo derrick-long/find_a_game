f0bfb250b7d6211d3f5872d33a5fc553ec0b74ea
derrick-long <long.derrick8@gmail.com>
2018-09-03 21:08:08 -0400

    JS updates

--- public/javascript/maps.js
+++ public/javascript/maps.js
@@ -17,7 +17,7 @@ function initMap() {
 
 //make an if statement only if my hidden attribs show
 function createMarker(latlng) {
-
+// watch for weird interaction with map center here
   if(marker != undefined && marker != ''){
     marker.setMap(null);
     marker = '';
@@ -40,13 +40,41 @@ function geocodeAddress() {
 		if (status == google.maps.GeocoderStatus.OK) {
 
       var myResult = results[0].geometry.location;
-
-      createMarker(myResult);
+			//clean up later
+      map = new google.maps.Map(document.getElementById("map"));
       map.setCenter(myResult);
 
-      map.setZoom(17);
+      map.setZoom(15);
 		}
   });
 }
+
+$(function(){
+      $('#submit').click(function(e){
+            e.preventDefault();
+            console.log('select_link clicked');
+
+
+						var data = {};
+            data.searchZip = '23505';
+
+                    //something is wrong with var here
+
+           $.ajax({
+  						type: 'GET',
+  						data: data,
+  				    contentType: 'application/json',
+              url: 'http://localhost:5000/games/endpoint',
+              success: function(response) {
+									response.games.forEach(function(game){
+										console.log(game.location.coordinates);
+									})
+                  //manipulate info here, pass to geoloc and we're in buisness baby
+                }
+              });
+		
+    });
+});
+
 //one big ajax request -> sends request to back end to find games,
 // then update map data
